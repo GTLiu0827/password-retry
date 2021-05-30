549e08e5940b9a84fbbc6d8d0d0e9e335ac19c3c
David Liu <fly82827@gmail.com>
2021-05-30 18:16:45 +0800

    code2

---
+++ password2.py
@@ -0,0 +1,12 @@
+password = 'a123456'
+count = 2
+while count < 3 :
+    code = input('請輸入密碼:')
+    if code == password:
+        print ('登入成功!')
+        break
+    elif count > 0 :
+        print ('密碼錯誤! 還有',count,'次機會')
+        count = count - 1
+    else:
+        break
